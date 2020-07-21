3f05ac28d16ef311bf4be9990ff7dbdc123c9363
Peter Bates <louisvillepeter@gmail.com>
2020-07-20 22:49:45 -0400

    adds clickSwitch.js and updates the rest of the files

---
+++ clickSwitch.js
@@ -0,0 +1,46 @@
+document.getElementById("zucchini1").addEventListener("click", writezucchini1 ) ;
+
+function  writezucchini1(){
+alert("April 25th is National Zucchini Bread Day.");
+}
+
+document.getElementById("zucchini2").addEventListener("click", writezucchini2 ) ;
+
+
+function  writezucchini2(){
+    alert("One zucchini is a “zucchina.”");
+    }
+
+    document.getElementById("zucchini3").addEventListener("click", writezucchini3 ) ;
+
+
+function  writezucchini3(){
+    alert("Zucchinis were first brought to the United States in the 1920s by the Italians");
+    }
+
+    document.getElementById("seagal1").addEventListener("click", writeseagal1 ) ;
+
+
+function  writeseagal1(){
+    alert("Seagal and Russian President Vladimir Putin have been friends for some time now");
+    }
+
+    document.getElementById("seagal2").addEventListener("click", writeseagal2 ) ;
+
+
+function  writeseagal2(){
+    alert("Seagal began his adult life as an Aikido instructor in Japan");
+    }
+
+    document.getElementById("beet1").addEventListener("click", writebeet1 ) ;
+
+
+function  writebeet1(){
+    alert("In ancient times, the root of the beet was not used for cooking but instead as a medicine for treating painful disorders at that time, like headaches and toothaches.");
+    }
+
+    document.getElementById("beet2").addEventListener("click", writebeet2 ) ;
+
+function  writebeet2(){
+    alert("The beet was initially cultivated around 2,000 BC in the Mediterranean region");
+    }
--- script.js
+++ script.js
@@ -1,213 +1,53 @@
 // script.js
-//
 
-const quest = document.querySelector ('#input-quest');
-const love = document.querySelector ('#input-love');
-const foe= document.querySelector ('#input-enemy');
-
-
-const food = document.querySelector ('#food');
-const job = document.querySelector ('#job');
-const equip = document.querySelector ('#equip');
-
+const name = document.querySelector ('#name');
+const vegtable = document.querySelector ('#vegtable');
+const email= document.querySelector ('#email');
+const fruit = document.querySelector ('#fruit');
 const story = document.querySelector ('#story');
 
-const input = document.querySelector('#form');
-const output = document.querySelector('#output');
+document.getElementById("button-submit").addEventListener("click", ValidateEmail, writeStory) ;
 
 
-const submit = document.querySelector('#button-cancel');
-submit.addEventListener("click",y) ;
+function ValidateEmail() {
 
+    var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-const submit = document.querySelector('#button-submit');
-submit.addEventListener("click",s) ;
-
-
-
-
-function y
-(){
- 
-var select = document.getElementById('food');
-var foodValue = parseInt(select.options[select.selectedIndex].value);
-
-var select = document.getElementById('job');
-var jobValue = parseInt(select.options[select.selectedIndex].value);
-
-var select = document.getElementById('equip');
-var equipValue = parseInt(select.options[select.selectedIndex].value);
-const roll = Math.abs( foodValue - equipValue + jobValue);
+    if (!filter.test(email.value)) {
+    alert('Please provide a valid email address');
+    email.focus;
+    return false;
+ }
 }
 
-function s(){
+document.getElementById("button-submit").addEventListener("click", writeStory) ;
 
-    
-if (roll===0){
+function Dropdown_Validation(ddlId) {
+    var empty = document.getElementById(ddlId).value;
+
+    if (empty == "0") {
+        alert('Please select an item');
+        return false;
+    }
+    return true;
+}
+
+function writeStory() {  
 
     let finishedStory = ""
-    finishedStory += "You have choosen to 0 " + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
+    finishedStory += "Hello " + name.value + " "
+    finishedStory += "I see your enjoy " + vegtable.value + " "
+    finishedStory += "and you also like " + fruit.value + " . "
+    finishedStory += "Can I email you at " + email.value + "  to offer you a great deal on some "+ fruit.value + " and some " + vegtable.value + "?"
 
     story.innerHTML = finishedStory
 }
-else if ( roll===1){
 
-    let finishedStory = ""
-    finishedStory += "You have choosen to 1 " + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===2){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 2 " + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===3){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 3 " + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===4){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 4" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===5){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to  5" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===6){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 6 " + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===7){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to  7" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===8){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 8" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===9){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 9" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===10){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 10" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===11){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 11" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else if ( roll===12){
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 12" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-else {
-
-    let finishedStory = ""
-    finishedStory += "You have choosen to 13" + quest.value + " . "
-    finishedStory += "You have choosen to " + job.value + " . "
-    finishedStory += "You have choosen to " + love.value + " . "
-    finishedStory += "You have choosen to " + equip.value + " . "
-    finishedStory += "You have choosen to " + quest.value + " . "
-
-    story.innerHTML = finishedStory
-}
-}
-// const again = document.querySelector('#again');
-// again.addEventListener("click", resetPage, false);
-// window.addEventListener("keydown", keydownHandler, false);
-
-// function keydownHandler(event) {
-//     console.log("Enter key pressed");
-// }
-// document.querySelector('#input-quest').focus();
-// const submit = document.getElementById('button-submit');
-
-console.log(roll);
- 
-// function checkComplete()
+document.getElementById("button-reset").onclick = function() {
+    document.getElementById("email").value = "";
+    document.getElementById("fruit").value = "";
+    document.getElementById("vegtable").value = "";
+    document.getElementById("name").value = "";
+    story.innerHTML  = "";
+  };
